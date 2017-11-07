e1ff0e3ced826afb43f43181adc97f47744f6c69
leemlam <liamuptonwalsh@gmail.com>
2017-11-06 18:32:44 -0800

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ function clicked() {
 
 function upgrade(name) {
   if (name === "clicker cat") {
-    if (money >= money) {
+    if (money >= catcost) {
       msec += 1;
       catown += 1
       money -= catcost;
