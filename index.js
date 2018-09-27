4ca3ef46127223ec082d5376168ed291a15d1fb0
leemlam <liamuptonwalsh@gmail.com>
2018-09-27 12:28:19 -0700

    Update index.js

--- index.js
+++ index.js
@@ -9,13 +9,15 @@ workerown = 0;
 upown = 0;
 catadd = 1;
 workadd = 15;
-
-window.onbeforeunload = closingCode;
+repeat = 1000;
+//save before exiting
 function closingCode(){
+  if (confirm("You have closed the window, would you like to save?") === true) {
    save();
    return null;
+  }
 }
-
+//updates all values
 function reloadall() {
   document.getElementById("click").innerHTML = "LB/click: " + moneyup + " | LB/sec: " + msec;
   document.getElementById("total").innerHTML = "LB: " + money;
@@ -23,7 +25,7 @@ function reloadall() {
   document.getElementById("worker").innerHTML = workerown + "-worker: " + workercost + " | +" + workadd + "/sec";
   document.getElementById("upgrade").innerHTML = upown + "-main upgrade: " + upcost;
 }
-
+//overwrites save file
 function save() {
   localStorage.setItem("money", money);
   localStorage.setItem("moneyup", moneyup);
@@ -36,9 +38,10 @@ function save() {
   localStorage.setItem("catown", catown);
   localStorage.setItem("workerown", workerown);
   localStorage.setItem("upown", upown);
-  
+  localStorage.setItem("catadd", catadd);
+  localStorage.setItem("workadd", workadd);
 }
-
+//loads save file
 function load() {
   money = parseInt(localStorage.getItem("money"));
   moneyup = parseInt(localStorage.getItem("moneyup"));
@@ -51,9 +54,12 @@ function load() {
   catown = parseInt(localStorage.getItem("catown"));
   workerown = parseInt(localStorage.getItem("workerown"));
   upown = parseInt(localStorage.getItem("upown"));
+  catadd = parseInt(localStorage.getItem("catadd"));
+  workadd = parseInt(localStorage.getItem("workadd"));
   
   reloadall()
 }
+//resets all values
 function reset() {
   if (confirm("Are you sure you want to reset?") === true) {
     money = 0;
@@ -65,27 +71,32 @@ function reset() {
     catown = 0;
     workerown = 0;
     upown = 0;
+    catadd = 1;
+    workadd = 15;
     reloadall()
   }
 }
-
+//timer
 function myTimer() {
-  money += msec;
+  if (msec <= 100) {money += msec; repeat = 1000;}
+  else if (msec <= 1000) {money += Math.round(msec/10); repeat = 100;}
+  else if (msec > 1000) {money += Math.round(msec/20); repeat = 50;}
+  
   document.getElementById("total").innerHTML = "LB: " + money;
 }
-setInterval(myTimer, 1000);
-
+setInterval(myTimer, repeat);
+//what happens when button is clicked
 function clicked() {
   money += moneyup;
   document.getElementById("total").innerHTML = "LB: " + money;
 }
-
+//upgrade function
 function upgrade(name) {
   if (name === "clicker cat") {
     if (money >= catcost) {
-      if (catown < 10) {msec += catadd; catadd ++;}
-      else if (catown < 25) {msec += 20*catadd; catadd ++} 
-      else if (catown < 50) {msec += 500*catadd; catadd ++}
+      if (catown <= 10) {msec += catadd; catadd ++;}
+      else if (catown <= 25) {msec += 20*catadd; catadd ++} 
+      else if (catown <= 50) {msec += 500*catadd; catadd ++}
       else {msec += 1500*catadd; catadd ++}
       catown += 1;
       money -= catcost;
